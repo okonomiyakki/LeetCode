76b7e65b0267cb4e9f5f3eb1d0b02cd25b294f06
jiwon <parkji12532@naver.com>
2023-08-23 15:48:59 +0900

    Time: 53 ms (69.27%), Space: 41.2 MB (97.37%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0
+    for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+        nums[i] = 0;
+    }
+    else k++
+}
+
+nums.sort((x, y) => y - x);
+
+return k
+};
+
+/*
+9. for문을 돌며 val과 같은 nums배열 요소의 값을 0으로 바꾼다.
+12. 같지 않은 경우를 카운트한다.
+15. nums배열을 내림차순으로 정렬한다.
+*/
